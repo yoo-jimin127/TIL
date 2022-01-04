75ca038b2176ed446e9c22740abde8687d62a648
yoo-jimin127 <dbwlals9936@naver.com>
2022-01-05 01:20:46 +0900

    add : chap 12 - add product

--- Python/CodeLion_Python/codelion.py
+++ Python/CodeLion_Python/codelion.py
@@ -1,8 +1,7 @@
 import random
 
-
-lunch = random.choice(["된장찌개", "피자", "제육볶음"])
-dinner = random.choice(["김밥","쫄면","돈까스"])
-
-infomation = {"고향":"수원", "취미":"영화관람", "좋아하는 음식":"초밥"}
-print(infomation)
+lunch = ["된장찌개", "피자", "제육볶음", "짜장면"]
+while True :
+    item = input("음식을 추가 해주세요 : ")
+    lunch.append(item)
+    print(lunch)
