9d4e6da3dc0a95f005dc63223983c33b4a36c87e
yoo-jimin127 <dbwlals9936@naver.com>
2022-01-05 01:24:33 +0900

    add : chap 13 - add/change set product

--- Python/CodeLion_Python/codelion.py
+++ Python/CodeLion_Python/codelion.py
@@ -1,7 +1,14 @@
 import random
 
 lunch = ["된장찌개", "피자", "제육볶음", "짜장면"]
+
 while True :
-    item = input("음식을 추가 해주세요 : ")
-    lunch.append(item)
     print(lunch)
+    item = input("음식을 추가 해주세요 : ")
+    if (item == "q") :
+        break
+    else :
+        lunch.append(item)
+
+print(lunch)
+set_lunch = set(lunch)
