4fbd2df742d14343b70c12ee49ef41f25d108c8a
yoo-jimin127 <dbwlals9936@naver.com>
2022-01-05 20:58:08 +0900

    add : print weather infomation

--- Python/Weather_Info_Python/weatherInfo.py
+++ Python/Weather_Info_Python/weatherInfo.py
@@ -11,5 +11,16 @@ result = requests.get(api)
 
 data = json.loads(result.text)
 
-print(type(result.text))
-print(type(data))
+# print(type(result.text))
+# print(type(data))
+
+print(data["name"], "의 날씨입니다.")
+print("날씨는 ", data["weather"][0]["main"], "입니다.")
+print("현재 온도는 ", data["main"]["temp"], "입니다.")
+print("체감 온도는 ", data["main"]["feels_like"],"입니다.")
+print("최저 기온은 ", data["main"]["temp_min"],"입니다.")
+print("최고 기온은 ", data["main"]["temp_max"],"입니다.")
+print("습도는 ", data["main"]["humidity"],"입니다.")
+print("기압은 ", data["main"]["pressure"],"입니다.")
+print("풍향은 ", data["wind"]["deg"],"입니다.")
+print("풍속은 ", data["wind"]["speed"],"입니다.")
