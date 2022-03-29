5e4b933a92f8b1eae6406a77d12d6e1bd6f8c830
yoo-jimin127 <dbwlals9936@naver.com>
2022-03-29 18:49:26 +0900

    add : react study - docs references

--- React/cra-env-project/test-app/src/App.js
+++ React/cra-env-project/test-app/src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Composition from './components/2-4.props/Composition';
+import Extraction from './components/2-4.props/Extraction/Extraction';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Extraction />
+      <Composition />
     </div>
   );
 }
---
+++ React/cra-env-project/test-app/src/components/2-4.props/Composition.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>
+}
+export default function Composition() {
+  return (
+    <div>
+        <Welcome name="Jimin" />
+    </div>
+  )
+}
