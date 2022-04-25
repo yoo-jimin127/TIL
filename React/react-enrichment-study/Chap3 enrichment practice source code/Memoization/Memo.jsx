ff5fb63d34d4957eff817a9edec8aa2707654126
yoo-jimin127 <dbwlals9936@naver.com>
2022-04-25 22:16:38 +0900

    add : memoization apply

--- React/react-enrichment-study/Chap3 enrichment practice source code/Memoization/Memo.jsx
+++ React/react-enrichment-study/Chap3 enrichment practice source code/Memoization/Memo.jsx
@@ -1,17 +1,28 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Comments from './Comments';
 
 
 const CommentList = [
     { title : "comment1", content: "message1", likes: 1 },
-    { title : "comment2", content: "message2", likes: 2 },
-    { title : "comment3", content: "message3", likes: 3 }
+    { title : "comment2", content: "message2", likes: 1 },
+    { title : "comment3", content: "message3", likes: 1 }
 ]
 export default function Memo() {
     const [comments, setComments] = useState(CommentList);
-    return (
-        <div>
-            <Comments commentList={comments} />
-        </div>
-    )
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setComments((prevComments) => [
+                ...prevComments,
+                {
+                    title : `comment1${prevComments.length + 1}`, content: `message${prevComments.length + 1}`, likes: 1
+                }
+            ])
+        }, 1000);
+        return () => {
+            clearInterval(interval);
+        }
+    }, [input])
+
+    return <Comments commentList={comments} />
 }
