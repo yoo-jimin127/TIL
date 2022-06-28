5532eea3c3a3fbce835e6616c1cfcc8fc470a9cc
yoo-jimin127 <dbwlals9936@naver.com>
2022-06-28 15:31:01 +0900

    add : Typescript 기본 지식 코드

--- Front-End/Typescript/interface.ts
+++ Front-End/Typescript/interface.ts
@@ -1,1 +1,91 @@
+interface PersonInterface {
+    name: string;
+    age: number;
+}
 
+function hello1(person: PersonInterface): void {
+    console.log(`Hello! This is ${person.name}!`);
+}
+
+// 인터페이스 생성
+const person1: PersonInterface = {
+    name: 'jimin',
+    age: 23,
+};
+
+hello1(person1);
+
+// Interface Optional 1
+interface PersonInterface_Option {
+    name: string;
+    age?: number;
+}
+
+function hello2(person: PersonInterface_Option) {
+    console.log(`Hello! This is ${person.name}!`)
+}
+
+const person2: PersonInterface_Option = {name: 'jimin', age: 23};
+const person3: PersonInterface_Option = {name: 'front'};
+
+hello2(person2);
+hello2(person3);
+
+// Interface Optional 2
+interface PersonInterface_Option2 {
+    name: string;
+    age?: number;
+    [index: string]: any;
+}
+
+function hello3(person: PersonInterface_Option2) {
+    console.log(`Hello! This is ${person.name}!`);
+}
+
+const person4 :PersonInterface_Option2 = {
+    name:'likelion',
+    part: 'front',
+};
+
+hello3(person4);
+
+// Interface Extends
+interface IPerson2 {
+    name: string;
+    age?: number;
+}
+
+interface IKorean extends IPerson2 {
+    city: string;
+}
+
+const koreanObj :IKorean = {
+    name: '유지민',
+    city: 'incheon',
+}
+
+// Interface class
+// 인터페이스 : 컴파일 시 사라짐 - 타입
+interface IPerson1 {
+    name: string;
+    age?: number;
+    hello(): void;
+}
+
+// 구현체 (Interface를 implements)
+class Person implements IPerson1 {
+    name: string;
+    age?: number | undefined;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    hello(): void {
+        console.log(`Hello! This is ${this.name}`);
+    }
+}
+
+const person7 = new Person('front');
+const person8:IPerson1 = new Person('likelion');
+person7.hello();
--- Front-End/Typescript/interface_function.ts
+++ Front-End/Typescript/interface_function.ts
@@ -1,1 +1,35 @@
+interface PersonInterface_Function {
+    name: string;
+    age: number;
+    hello(): void;
+}
 
+const person5: PersonInterface_Function = {
+    name: 'front',
+    age: 10,
+    hello: function() :void {
+        console.log(`hello! This is ${this.name}!`);
+    }
+}
+
+const person6: PersonInterface_Function = {
+    name: 'front2',
+    age: 10,
+    hello() :void {
+        console.log(`hello! This is ${this.name}!`);
+    }
+}
+
+interface HelloPerson {
+    (name: string, age?: number): void;
+}
+
+const helloPerson: HelloPerson = function(name: string, age?:number) {
+    console.log(`Hello! This is ${name}!`)
+}
+
+helloPerson('jimin', 39);
+helloPerson('front');
+
+person5.hello();
+person6.hello();
